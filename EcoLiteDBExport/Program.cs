9e786734b2532aff7f917938cc55f38e7b95133e
EdwinExtendas <edwin.ten.brinke@extendas.com>
2020-01-30 23:47:45 +0100

    master update

--- EcoLiteDBExport/Program.cs
+++ EcoLiteDBExport/Program.cs
@@ -3,7 +3,9 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
+using System.Text.RegularExpressions;
 
 namespace EcoLiteDBExport
 {
@@ -28,6 +30,7 @@ namespace EcoLiteDBExport
 
     public class config
     {
+        public string api_access_token { get; set; }
         public int timeout { get; set; }
         public int query_limit { get; set; }
         public string api_url { get; set; }
@@ -52,13 +55,14 @@ namespace EcoLiteDBExport
         }
 
         // post the data to the api
-        static void postData(string api_url, Dictionary<string, List<data_models>> post_data)
+        static void postData(string api_url, Dictionary<string, List<data_models>> post_data, config config_data)
         {
             log(string.Format("posting data to {0}", api_url));
             using (WebClient wc = new WebClient())
             {
                 // set it so the api knows that type of data is being received
                 wc.Headers[HttpRequestHeader.ContentType] = "application/json";
+                wc.Headers[HttpRequestHeader.Authorization] = config_data.api_access_token;
                 // post to the api & transform the api array to JSON
                 string HtmlResult = wc.UploadString(
                     api_url,
@@ -97,13 +101,26 @@ namespace EcoLiteDBExport
 
         static void processNewRecords(config config_data)
         {
-
             // get the previous run data
             string previous_run_file_location = string.Format("{0}{1}", AppDomain.CurrentDomain.BaseDirectory, previous_run_file_name);
             List<previous_run_data> previous_run_data = getPreviousRunData(previous_run_file_location, config_data);
 
+            DirectoryInfo backup_directory = new DirectoryInfo(config_data.game_database_file);
+            var storage_directories = Directory.GetDirectories(string.Format("{0}/Storage", config_data.game_database_file));
+            foreach (string storage in storage_directories)
+            {
+                if (!storage.Contains("Backup"))
+                {
+                    continue;
+                }
+
+                backup_directory = new DirectoryInfo(string.Format("{0}/Storage/Backup", config_data.game_database_file)).GetDirectories()
+                    .OrderByDescending(f => f.LastWriteTime)
+                    .First();
+            }
+
             // connect to database
-            var db = new LiteDatabase(config_data.game_database_file);
+            var db = new LiteDatabase(backup_directory.FullName + "/Game.db");
 
             log("---------------------------");
             // initiate the response array
@@ -121,6 +138,15 @@ namespace EcoLiteDBExport
                 // set index on _id
                 collection.EnsureIndex(x => x._id);
 
+                // if its lower than the previous_run.id there has been a reset
+                // use this id as the new id
+                var latest = collection.FindOne(Query.All(Query.Descending));
+                if (latest != null && latest._id < previous_run.id)
+                {
+                    log("an older database has been placed");
+                    previous_run.id = latest._id;
+                }
+
                 log(string.Format("executing query for {0} collection", previous_run.name));
                 // execute query to get all rows since id
                 var query = collection.Find(x => x._id > previous_run.id, limit: config_data.query_limit);
@@ -131,19 +157,21 @@ namespace EcoLiteDBExport
                     previous_run.id = entry._id;
                 }
 
-                log("writing last_id to the previous_id file");
-                // write the last id gotten from the query to the file
-                // so the next time the script runs it doesn't have to get all the current & previous records
-                File.WriteAllText(previous_run_file_location, JsonConvert.SerializeObject(previous_run_data));
-
 
                 // add total result to the main data array that will be sent to the api
                 api_data.Add(previous_run.name, result);
                 log("---------------------------");
             }
+            // close database connection
+            db = null;
 
-            // get api url
-            postData(config_data.api_url, api_data);
+            // post the data to the api
+            postData(config_data.api_url, api_data, config_data);
+
+            log("writing last_id to the previous_id file");
+            // write the last id gotten from the query to the file
+            // so the next time the script runs it doesn't have to get all the current & previous records
+            File.WriteAllText(previous_run_file_location, JsonConvert.SerializeObject(previous_run_data));
         }
 
         static void Main(string[] args)
@@ -151,7 +179,7 @@ namespace EcoLiteDBExport
             while(true)
             {
                 // loop every x minutes
-                log("*************************");
+                log("***************************");
                 try
                 {
                     // get config data
